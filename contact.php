5b2ab41c42a1956afa48af538ae2dce5b1927fff
Ash111-max <dsilvaastle111@gmail.com>
2024-10-30 23:19:25 +0530

    updated php without captcha

--- contact.php
+++ contact.php
@@ -22,37 +22,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $likedSectionComment = $_POST["likedSectionComment"];
     $improvementSectionComment = $_POST["improvementSectionComment"];
     $rating = $_POST["rating"];
-    $recaptcha = $_POST['g-recaptcha-response'];
-    $secret_key = '6Ldrg1gqAAAAAAUTe2imQSGfy0SnyP2xgdoJpx5F';
 
-    // Verify the reCAPTCHA response
-    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret_key}&response={$recaptcha}");
-    $response_keys = json_decode($response, true);
+    // Prepare and bind the SQL statement
+    $stmt = $conn->prepare("INSERT INTO feedback_table (name, email, contact_number, liked_section, improvement_section, liked_section_comment, improvement_section_comment, rating)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssssss", $name, $email, $contactNumber, $likedSection, $improvementSection, $likedSectionComment, $improvementSectionComment, $rating);
 
-    if (intval($response_keys["success"]) !== 1) {
-        echo "Please complete the CAPTCHA.";
+    if ($stmt->execute() === TRUE) {
+        echo "Thank you for your feedback!";
+        echo "<br>Name: $name";
+        echo "<br>Email: $email";
+        echo "<br>Contact Number: $contactNumber";
+        echo "<br>Liked Section: $likedSection";
+        echo "<br>Improvement Section: $improvementSection";
+        echo "<br>Liked Section Comment: $likedSectionComment";
+        echo "<br>Improvement Section Comment: $improvementSectionComment";
+        echo "<br>Rating: $rating";
     } else {
-        // Prepare and bind the SQL statement
-        $stmt = $conn->prepare("INSERT INTO feedback_table (name, email, contact_number, liked_section, improvement_section, liked_section_comment, improvement_section_comment, rating)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("ssssssss", $name, $email, $contactNumber, $likedSection, $improvementSection, $likedSectionComment, $improvementSectionComment, $rating);
-
-        if ($stmt->execute() === TRUE) {
-            echo "Thank you for your feedback!";
-            echo "<br>Name: $name";
-            echo "<br>Email: $email";
-            echo "<br>Contact Number: $contactNumber";
-            echo "<br>Liked Section: $likedSection";
-            echo "<br>Improvement Section: $improvementSection";
-            echo "<br>Liked Section Comment: $likedSectionComment";
-            echo "<br>Improvement Section Comment: $improvementSectionComment";
-            echo "<br>Rating: $rating";
-        } else {
-            echo "Error: " . $stmt->error;
-        }
-
-        $stmt->close();
+        echo "Error: " . $stmt->error;
     }
+
+    $stmt->close();
 }
 
 $conn->close();
